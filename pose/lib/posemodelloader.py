3924acfb7f4c0b1ad9fbb999790a1b56c2315670
Leander Peter <kontakt@leanderpeter.de>
2021-03-11 12:20:16 +0100

    script -> oop

---
+++ pose/lib/posemodelloader.py
@@ -0,0 +1,41 @@
+
+import sys
+
+'''
+import os.path as path
+
+two_up =  path.abspath(path.join(__file__ ,"../../.."))
+
+print(two_up)
+'''
+
+sys.path.append('../')
+
+
+for i in sys.path:
+    print(i)
+
+
+from lib.network.rtpose_vgg import get_model
+
+class posemodelloader:
+
+    def __init__(self):
+        super().__init__()
+        self._model_path = None
+        self._classifier = None
+        self._input_size = None
+        self._output_size = None
+
+    def load_model(self):
+        self._model = get_model()
+        self._model.load_state_dict(torch.load(_model_path))
+
+        #Switch between CUDA or CPU
+        self._model = torch.nn.DataParallel(model).cuda()
+        #model = torch.nn.DataParallel(model)
+        self._model.float()
+        self._model.eval()
+
+        return self._model
+
